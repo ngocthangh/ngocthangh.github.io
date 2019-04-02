ade566a683355bc9c24090d40602b92607d77198
NgocThang <ngocthanguit@gmail.com>
2019-04-02 17:40:47 +0700

    test react

---
+++ precache-manifest.6dc6989fc475d9ff3a76108dc5af5fb2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2fb641cb4ff106d4ac36",
+    "url": "/static/js/main.eb01ac16.chunk.js"
+  },
+  {
+    "revision": "d56a7de8eb7d9214642a",
+    "url": "/static/js/2.ba635f22.chunk.js"
+  },
+  {
+    "revision": "fca405fe725897c88f8a41658ee83c6d",
+    "url": "/index.html"
+  }
+];
